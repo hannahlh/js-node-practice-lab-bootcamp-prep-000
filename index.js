09112b806d8e45e9e6a0d4e2593edb5334bb4ef0
Hannah Hughes <hugheshannah96@gmail.com>
2018-05-22 22:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Hannah"
-var height = "63"
+var height = "74"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
